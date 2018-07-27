a32a2cc46dfffe4a59e207127671fde6ace2e7e5
Unknown <krystian.gizynski@asseco.pl>
2018-07-27 18:29:50 +0200

    commit

---
+++ demo/src/main/groovy/com/example/demo/models/User.groovy
@@ -0,0 +1,20 @@
+package com.example.demo.models
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import org.springframework.data.annotation.Id
+
+import javax.validation.constraints.NotNull
+
+class User {
+    
+    @Id
+    String id
+    
+    @NotNull
+    String username
+    
+    @NotNull
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    String password
+    
+}
